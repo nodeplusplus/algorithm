db0eb839a5929fe96cfccece7e12c9dbff292ab0
Tuan Nguyen <tuan.nguyen@nodeplusplus.com>
2020-11-04 09:53:40 +0700

    improve leetcode solution

--- leetcode/00050.powx-n.py
+++ leetcode/00050.powx-n.py
@@ -3,28 +3,37 @@ from decimal import Decimal
 
 class Solution:
     def myPow(self, x: float, n: int) -> float:
+        # Check some edeges case
         if n == 0:
             return 1
+        if n == 1:
+            return x
 
-        r = self.pow(x, abs(n))
+        def pow(x: float, n: int) -> float:
+            if n == 0:
+                return 1
+            if n == 1:
+                return x
+
+            is_even = n % 2 == 0
+            pr = pow(x, int(n/2))
+            prr = pr*pr
+
+            if is_even:
+                return prr
+            else:
+                return x * prr
+
+        r = pow(x, abs(n))
+
         return r if n > 0 else 1/r
-
-    def pow(self, x: float, n: int) -> float:
-        if n == 0:
-            return 1
-
-        is_even = n % 2 == 0
-
-        if is_even:
-            return self.pow(x, int(n/2)) * self.pow(x, int(n/2))
-        else:
-            return x * self.pow(x, int(n/2)) * self.pow(x, int(n/2))
 
 
 tests = [
     ([float(2), 10], 1024),
     ([float(2.1), 3], 9.26100),
-    ([float(2), -2], 1/4)
+    ([float(2), -2], 1/4),
+    ([float(0.00001), 2147483647], 0),
 ]
 
 
