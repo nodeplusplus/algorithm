d64f7d5aaa7ae4b1e16925f79638e5e52781b438
Tuan Nguyen <tuan.nguyen@nodeplusplus.com>
2020-11-04 09:23:52 +0700

    add leetcode solution

--- leetcode/00050.powx-n.py
+++ leetcode/00050.powx-n.py
@@ -6,17 +6,25 @@ class Solution:
         if n == 0:
             return 1
 
+        r = self.pow(x, abs(n))
+        return r if n > 0 else 1/r
+
+    def pow(self, x: float, n: int) -> float:
+        if n == 0:
+            return 1
+
         is_even = n % 2 == 0
 
         if is_even:
-            return self.myPow(x, int(n/2)) * self.myPow(x, int(n/2))
+            return self.pow(x, int(n/2)) * self.pow(x, int(n/2))
         else:
-            return x * self.myPow(x, int(n/2)) * self.myPow(x, int(n/2))
+            return x * self.pow(x, int(n/2)) * self.pow(x, int(n/2))
 
 
 tests = [
     ([float(2), 10], 1024),
-    # ([float(2.1), 3], 9.26100)
+    ([float(2.1), 3], 9.26100),
+    ([float(2), -2], 1/4)
 ]
 
 
