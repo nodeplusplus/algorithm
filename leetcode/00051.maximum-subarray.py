5f4a77bec5aa80128beed1352964d729c9fd25d5
Tuan Nguyen <tuan.nguyen@nodeplusplus.com>
2020-11-06 09:24:18 +0700

    add dynamic programming

--- leetcode/00051.maximum-subarray.py
+++ leetcode/00051.maximum-subarray.py
@@ -16,29 +16,41 @@ from typing import List
 #         return ans
 
 # Another solution, fater than 5%
+# class Solution:
+#     def maxSubArray(self, nums: List[int]) -> int:
+#         maxsofar = 0
+#         a = []
+#         maxending = 0
+#         for i in range(len(nums)):
+#             maxending = maxending + nums[i]
+#             if maxending < 0:
+#                 a.append(maxending)
+#                 maxending = 0
+#             elif maxsofar < maxending:
+#                 maxsofar = maxending
+#         if maxsofar == 0 and a != []:
+#             return max(nums)
+#         else:
+#             return maxsofar
+
+# Dynamic Programming
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        maxsofar = 0
-        a = []
-        maxending = 0
-        for i in range(len(nums)):
-            maxending = maxending + nums[i]
-            if maxending < 0:
-                a.append(maxending)
-                maxending = 0
-            elif maxsofar < maxending:
-                maxsofar = maxending
-        if maxsofar == 0 and a != []:
-            return max(nums)
-        else:
-            return maxsofar
+        if len(nums) <= 1:
+            return sum(nums)
+
+        ans = [*nums]
+        for i in range(1, len(ans)):
+            ans[i] = max(ans[i-1]+nums[i], nums[i])
+
+        return max(ans)
 
 
 tests = [
     ([-2, 1, -3, 4, -1, 2, 1, -5, 4], 6),
-    # ([-2, 1], 1),
-    # ([-1, 0, -2], 0),
-    # ([-2, -1, -2], -1)
+    ([-2, 1], 1),
+    ([-1, 0, -2], 0),
+    ([-2, -1, -2], -1)
 ]
 
 
