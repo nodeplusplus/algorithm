ab6db6e4c6ae01d9986b4b1f532a4d5107135f4c
Tuan Nguyen <tuan.nguyen@nodeplusplus.com>
2020-11-10 10:29:44 +0700

    add leetcode solution

---
+++ leetcode/00052.spiral-matrix.py
@@ -0,0 +1,82 @@
+from typing import List
+
+# class Solution(object):
+#     def spiralOrder(self, matrix):
+#         if not matrix: return []
+#         R, C = len(matrix), len(matrix[0])
+#         seen = [[False] * C for _ in matrix]
+#         ans = []
+#         dr = [0, 1, 0, -1]
+#         dc = [1, 0, -1, 0]
+#         r = c = di = 0
+#         for _ in range(R * C):
+#             ans.append(matrix[r][c])
+#             seen[r][c] = True
+#             cr, cc = r + dr[di], c + dc[di]
+#             if 0 <= cr < R and 0 <= cc < C and not seen[cr][cc]:
+#                 r, c = cr, cc
+#             else:
+#                 di = (di + 1) % 4
+#                 r, c = r + dr[di], c + dc[di]
+#         return ans
+
+class Solution(object):
+    def spiralOrder(self, matrix):
+        def spiral_coords(r1, c1, r2, c2):
+            for c in range(c1, c2 + 1):
+                yield r1, c
+            for r in range(r1 + 1, r2 + 1):
+                yield r, c2
+            if r1 < r2 and c1 < c2:
+                for c in range(c2 - 1, c1, -1):
+                    yield r2, c
+                for r in range(r2, r1, -1):
+                    yield r, c1
+
+        if not matrix: return []
+        ans = []
+        r1, r2 = 0, len(matrix) - 1
+        c1, c2 = 0, len(matrix[0]) - 1
+        while r1 <= r2 and c1 <= c2:
+            for r, c in spiral_coords(r1, c1, r2, c2):
+                ans.append(matrix[r][c])
+            r1 += 1; r2 -= 1
+            c1 += 1; c2 -= 1
+        return ans
+        
+tests = [
+    # (
+    #     [
+    #         [1, 2, 3],
+    #         [4, 5, 6],
+    #         [7, 8, 9]
+    #     ],
+    #     [1, 2, 3, 6, 9, 8, 7, 4, 5]
+    # ),
+    # (
+    #     [
+    #         [1, 2, 3, 4],
+    #         [5, 6, 7, 8],
+    #         [9, 10, 11, 12]
+    #     ],
+    #     [1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7]
+    # ),
+    (
+        [
+            [1, 2, 3, 4],
+            [5, 6, 7, 8],
+            [9, 10, 11, 12],
+            [13, 14, 15, 16],
+        ],
+        [1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 6, 7, 11, 10]
+    )
+]
+
+
+for A, r in tests:
+    a = Solution().spiralOrder(A)
+
+    if a != r:
+        print(f"\033[91m -> {a} # {r}")
+    else:
+        print(f"\033[92m -> OK")
