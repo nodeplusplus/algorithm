929623a05919341a563412b5b98d808bcaef36d0
Tuan Nguyen <tuan.nguyen@nodeplusplus.com>
2020-11-12 11:25:35 +0700

    add additional solution to hackerrank problem

--- hackerrank/00001.kangaroo.js
+++ hackerrank/00001.kangaroo.js
@@ -1,12 +1,28 @@
+// function kangaroo(x1, v1, x2, v2) {
+//   if (x1 < x2 && v1 <= v2) return "NO";
+//   if ((x1 - x2) % (v2 - v1) === 0) return "YES";
+//   return "NO";
+// }
+
 function kangaroo(x1, v1, x2, v2) {
   if (x1 < x2 && v1 <= v2) return "NO";
-  if ((x1 - x2) % (v2 - v1) === 0) return "YES";
-  return "NO";
+  if (x1 == x2) return "YES";
+
+  const dict = Math.abs(x1 - x2);
+  while (true) {
+    x1 += v1;
+    x2 += v2;
+    if (Math.abs(x1 - x2) >= dict) break;
+  }
+
+  return Math.abs(x1 - x2) == dict ? "YES" : "NO";
 }
 
 const tests = [
-  [[0, 3, 4, 2], "YES"],
-  [[0, 2, 5, 3], "NO"],
+  // [[0, 3, 4, 2], "YES"],
+  // [[0, 3, 0, 2], "YES"],
+  // [[0, 2, 5, 3], "NO"],
+  [[0, 4, 4, 1], "NO"],
 ];
 
 for (const [input, output] of tests) {
